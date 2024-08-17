627315d39dfe07671109c8476b0b6dcb0993a453
jonibekyorkulov <jonibekyorkulov@gmail.com>
2024-08-17 05:52:00 +0500

    add admin

--- OnlineShop/jazzmin_conf.py
+++ OnlineShop/jazzmin_conf.py
@@ -136,11 +136,11 @@ JAZZMIN_SETTINGS = {
     # - vertical_tabs
     # - collapsible
     # - carousel
-    "changeform_format": "vertical_tabs",
+    "changeform_format": "horizontal_tabs",
     # override change forms on a per modeladmin basis
     "changeform_format_overrides": {
         "auth.user": "collapsible",
-        "auth.group": "vertical_tabs",
+        "auth.group": "horizontal_tabs",
     },
     # Add a language dropdown into the admin
     # "language_chooser": True,
